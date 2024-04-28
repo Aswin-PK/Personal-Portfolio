23bcee1853d44f092b5d5d028f6b190181867039
Aswin P K <97808507+Aswin-PK@users.noreply.github.com>
2024-04-28 18:40:57 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='h-[4rem] w-full bg-gray-800 text-gray-400 flex items-center justify-center text-center px-[2rem] md:px-[6rem] 2xl:px-[10rem]'>
-        <p className='text-[0.6rem] md:text-[1rem]'>&copy; Copyright 2023.&nbsp; Designed and Developed by Aswin P K</p>
+        <p className='text-[0.6rem] md:text-[1rem]'>&copy; 2023.&nbsp; Designed and Developed by Aswin P K</p>
     </div>
   )
 }
