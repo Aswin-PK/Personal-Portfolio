e9fcccb34da385fa8114b928c8a606eec9ccd31e
Aswin P K <97808507+Aswin-PK@users.noreply.github.com>
2024-08-18 03:43:23 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,7 +6,7 @@ function About() {
           <div className="flex flex-col gap-3">
               <span className='text-gray-400 text-2xl md:text-4xl'>About Me</span>
               <div>
-                  <p className='text-gray-700 max-w-full md:max-w-[70rem] text-sm md:text-md lg:text-lg 2xl:text-xl mt-2 md:mt-6 mb-1 md:mb-2 text-justify'>
+                  <p className='text-gray-700 max-w-full md:max-w-[70rem] text-sm md:text-md lg:text-lg 2xl:text-xl mt-2 md:mt-6 mb-2 md:mb-4 text-justify'>
                       A passionate Frontend Developer with expertise in Vue.js, React.js, and Nuxt.js, I love creating dynamic, responsive web experiences that blend design and functionality. With a Master's degree in Computer Application, I have a solid foundation in programming and problem-solving, making it easy to pick up new languages and technologies. I’m always eager to learn, innovate, and collaborate on exciting projects.
                   </p>
                   <strong className="text-sm md:text-md lg:text-lg 2xl:text-xl">Let’s build something incredible together!</strong>
